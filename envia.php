83e4bcf27a2be473aa71139ff602942789e473db
redfelipefarias <142460742+redfelipefarias@users.noreply.github.com>
2024-04-23 23:32:06 -0300

    Update envia.php

---
+++ envia.php
@@ -0,0 +1,21 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone =addslashes($_POST['celular']);
+    $texto = $_POST['Sua mensagem:'];
+
+    $para = "redfelipefarias@hotmail.com";
+    $assunto = "Coleta de dados";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone;
+
+    $cabeca = "From: redfelipefarias@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!!");
+    }else{
+        echo("Houve um erro ao enviar o e-mail!")
+    }
+
+?>
